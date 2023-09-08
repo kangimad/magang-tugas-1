f1dc1cfdaaec4295069ae5ba224110d41ccd6c16
Imaduddin <imadtamvan@gmail.com>
2023-09-08 11:35:34 +0700

    08SEPT23

--- resources/views/home/index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts')
-@section('content')
-    <div class="mx-3 py-5 font-serif">
-        <div class="flex justify-center items-center">
-            <h1 class="block font-bold text-xl">{{ $title }}</h1>
-        </div>
-    </div>
-@endsection
