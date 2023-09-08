b89052fad0acd6a84dd8e71e52c8f318602c1d82
kangimad <imadtamvan@gmail.com>
2023-09-09 00:32:56 +0700

    user-layout (dashboard)

--- resources/views/user/partials/sidebar.blade.php
+++ resources/views/user/partials/sidebar.blade.php
@@ -1,11 +1,22 @@
 <div class="flex-col w-2/12 bg-cyan-950 border border-orange-600 p-1">
             <div class="flex-row w-full px-5 py-5">
-                <p class="text-gray-100 font-bold tracking-widest text-xl">Dashboard</p>
+                <p class="text-gray-100 font-bold tracking-widest text-xl">User Panel</p>
             </div>
 
             <div class="flex-row w-full px-5 py-3">
                 <div class="border border-white p-1 mb-2">
-                    <p class="text-gray-100 mb-2 font-normal tracking-widest text-md">Menu 1</p>
+                    <p class="text-gray-100 mb-2 font-normal tracking-widest text-md">Dashboard</p>
+
+                    <div class="border border-teal-300 p-1">
+                        <ul>
+                            <li class="mx-5 text-gray-100">Home</li>
+                            <li class="mx-5 text-gray-100">About</li>
+                            <li class="mx-5 text-gray-100">Contact</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="border border-white p-1 mb-2">
+                    <p class="text-gray-100 mb-2 font-normal tracking-widest text-md">Organization</p>
 
                     <div class="border border-teal-300 p-1">
                         <ul>
@@ -17,7 +28,7 @@
                     </div>
                 </div>
                 <div class="border border-white p-1 mb-2">
-                    <p class="text-gray-100 mb-2 font-normal tracking-widest text-md">Menu 1</p>
+                    <p class="text-gray-100 mb-2 font-normal tracking-widest text-md">Group</p>
 
                     <div class="border border-teal-300 p-1">
                         <ul>
@@ -29,7 +40,7 @@
                     </div>
                 </div>
                 <div class="border border-white p-1 mb-2">
-                    <p class="text-gray-100 mb-2 font-normal tracking-widest text-md">Menu 1</p>
+                    <p class="text-gray-100 mb-2 font-normal tracking-widest text-md">Type</p>
 
                     <div class="border border-teal-300 p-1">
                         <ul>
@@ -40,8 +51,8 @@
                         </ul>
                     </div>
                 </div>
-                <div class="border border-white p-1 mb-2">
-                    <p class="text-gray-100 mb-2 font-normal tracking-widest text-md">Menu 1</p>
+                <div class="border border-white p-1">
+                    <p class="text-gray-100 mb-2 font-normal tracking-widest text-md">Class</p>
 
                     <div class="border border-teal-300 p-1">
                         <ul>
